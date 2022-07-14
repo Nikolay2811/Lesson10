27bfd6dabf8500ed3f70d412f8fc1b73165ef5b1
Nikolay <nikola05@list.ru>
2022-07-14 17:50:18 +0300

    Добавил ДЗ

---
+++ ДЗ/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 1: Есть монотонная последовательность, каждое число встречается 
+//ровно то количество раз, какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,... 
+//Выведите ряд этой последовательности до N-го значения, желательно использовать рекурсию:
+//N = 12 -> "1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5"
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+
+void PrintPosl (int number)
+{
+if( number ==0)
+{
+    return;
+}
+
+
+
+PrintPosl(number-1);
+
+for (int i=0; i < number; i++)
+{
+    Console.Write($"{number}; ");
+}
+}
+
+PrintPosl(Prompt("Ведите число => "));
---
+++ ДЗ/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+//m = 3, n = 2 -> A(m,n) = 29
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int Acr(int m, int n)
+{
+    if (m == 0)
+    {
+        return n+1;
+    }
+    else if (m >0 && n==0)
+    {
+        return (Acr(m-1, 1));
+    }
+    else if(m > 0 && n > 0)
+    {return Acr(m-1, Acr(m, n-1));}
+    else return n+1;
+
+}
+int m = Prompt( "Ведите не отрицательное число  M => ");
+int n = Prompt( "Ведите не отрицательное число  N => ");
+Console.Write(Acr(m, n));
